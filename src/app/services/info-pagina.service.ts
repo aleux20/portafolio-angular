3c2f6c10ae56ef7f49b35a237cb463347f14ac3b
Alejandro Calderon <hansalejandroca@gmail.com>
2021-04-20 13:57:50 -0500

    Fin sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,17 +8,30 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
   info: InfoPagina= {};
   cargada= false;
+  equipo: any[]= [];
 
   constructor(private http: HttpClient) { 
-    // console.log('Servicio de InfoPagina listo');
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
+  private cargarInfo(){
     // Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
     .subscribe( (resp: InfoPagina) => {
-      this.cargada= true;
-      this.info= resp;
-
-      console.log('resp');
+    this.cargada= true;
+    this.info= resp;
     });
   }
-}
+
+  private cargarEquipo(){
+    // Leer el archivo JSON
+    this.http.get('https://angular-html-a9295-default-rtdb.firebaseio.com/equipo.json')
+    .subscribe( (resp: any) => {
+    this.equipo= resp;
+
+    // console.log('resp');
+    });
+    // this.equipo= resp;
+   }
+  }
